432e465ad90fa348340002c50252e2b5be1cc9c9
Tony Schappaugh <tony.schappaugh@intouchsol.com>
2023-05-30 17:20:07 -0500

    Working Version with Node 16

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a8537c364589da1097ad8e126dc35d47');
