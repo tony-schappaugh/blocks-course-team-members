7b65af4cba3c1435ee72ef1f7714f8a4b9529c9e
Tony Schappaugh <tony.schappaugh@intouchsol.com>
2023-06-21 13:09:13 -0500

    lesson 55

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '342dc9e03b553203aada');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9296a3aa033dbf8f235');
